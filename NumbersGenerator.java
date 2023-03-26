7f6074a2afd997b70593872312b871626f55bec9
daryl-goh <goh.daryl@gmail.com>
2023-03-26 22:57:30 +0800

    Reduced Numbers Generated from 7 to 6 for NumbersGenerator

--- NumbersGenerator.java
+++ NumbersGenerator.java
@@ -16,7 +16,7 @@ public class NumbersGenerator {
     Random randomizer = new Random();
     
     //For Loop to generate 7 numbers (6 + 1 additional number)
-    for (int i = 0; i < 7; i++)
+    for (int i = 0; i < 6; i++)
         while (true){
             //Randomizes number in nextInt between 0 and one less than the number in the bracket. e.g. 49 will randomize between 0 to 48
             //We want random number between 1 to 49, hence this will randomize between (0 to 48) PLUS 1
